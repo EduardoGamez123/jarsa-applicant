3bbea5f3eab2083002f7052cf4b14297cf8d20b2
EduardoGamez123 <39348698+EduardoGamez123@users.noreply.github.com>
2018-05-16 21:19:35 -0500

    Update primes.py

--- primes.py
+++ primes.py
@@ -13,4 +13,13 @@ class PrimeClass(object):
         Your method documentation here
         """
         # your primes code here
-        return "not implement yet"  # Remove this dummy line
+a=0
+num_int=int(input("Ingrese numero\n"))
+for i in range(1,num_int+1):
+ if(num_int % i==0):
+  a=a+1
+if(a!=2):
+ print(False)
+else:
+ print(True)     
+        #return "not implement yet"  # Remove this dummy line
